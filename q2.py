ad067894fbd421bd65ade00aaa4369b41a30c3ab
CeydaEkinAlpat <calpat24@ku.edu.tr>
2026-03-12 20:17:04 +0300

    Update q2.py

--- q2.py
+++ q2.py
@@ -2,8 +2,39 @@ def polynomial(x):
     # Don't change this!
     return 3 * x ** 2 - 4 * x - 5
 
+
+
+
+
+
 def bisection_root(a, b, epsilon=0.0001):
     assert b > a, 'Invalid range'
     assert polynomial(a) * polynomial(b) < 0, 'The bisection method cannot be applied'
     
-    # Implement here
+    
+    
+    
+    
+    
+    
+    
+def polynomial(x):
+    return 3 * x ** 2 - 4 * x - 5
+
+def bisection_root(a, b, epsilon=0.0001):
+    assert b > a, 'Invalid range'
+    assert polynomial(a) * polynomial(b) < 0, 'The bisection method cannot be applied'
+    for i in range(1000):
+        m = (a + b) / 2
+        
+        if abs(polynomial(m)) < epsilon:
+            return m
+
+        if polynomial(a) * polynomial(m) < 0:
+            b = m
+        else:
+            a = m
+            
+    return (a + b) / 2 
+
+
